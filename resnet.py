06d01ca22e3b9fadb20a8ac34a677e091e99c79c
Mohammad Hadi Salari <msalari@cedar1.cedar.computecanada.ca>
2020-05-04 16:53:08 -0700

    Bug fix

--- resnet.py
+++ resnet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .utils import MultiHeadLinear, init_module
+from utils import MultiHeadLinear, init_module
 
 __all__ = ['resnet']
 
